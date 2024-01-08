0728a10e069d47def193c1f7886b16ff7dce8442
OgataiKhan <badellino.kevin@gmail.com>
2024-01-08 16:07:52 +0100

    feat: style

--- index.php
+++ index.php
@@ -4,15 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
+  <!-- Google font: Roboto -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <!-- /Google font: Roboto -->
+  <!-- My css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- /My css -->
 </head>
 <body>
-  <h1>PHP Badwords</h1>
-  <form action="output.php" method="GET">
-    <label for="paragraph">Paragraph:</label>
-    <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Enter a sentence or paragraph..."></textarea>
-    <label for="censorword">Word to censor:</label>
-    <input type="text" name="censorword" id="censorword" placeholder="Enter the word you wish to censor...">
-    <button>Submit</button>
-  </form>
+  <main>
+    <h1>PHP Badwords</h1>
+    <form action="output.php" method="GET">
+      <label for="paragraph">Paragraph</label>
+      <textarea name="paragraph" id="paragraph" class="input-field" cols="30" rows="10" placeholder="Enter a sentence or paragraph..."></textarea>
+      <label for="censorword">Word to censor</label>
+      <input type="text" name="censorword" id="censorword" class="input-field" placeholder="Enter the word you wish to censor...">
+      <button class="btn">Submit</button>
+    </form>
+  </main>
 </body>
 </html>
--- output.php
+++ output.php
@@ -10,18 +10,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords - output</title>
+  <!-- Google font: Roboto -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <!-- /Google font: Roboto -->
+  <!-- My css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- /My css -->
 </head>
 <body>
-  <h1>PHP Badwords</h1>
-  <h2>The paragraph you entered is:</h2>
-  <p><?php echo $paragraph ?></p>
-  <h3>Length:</h3>
-  <p><?php echo strlen($paragraph) ?> characters</p>
-  <h2>The word you wish to censor is:</h2>
-  <p><?php echo $censor_word ?></p>
-  <h2>Here is the censored paragraph:</h2>
-  <p><?php echo $censored_paragraph ?></p>
-  <h3>Length:</h3>
-  <p><?php echo strlen($censored_paragraph) ?> characters</p>
+  <main>
+    <h1>PHP Badwords</h1>
+    <h2>The paragraph you entered is:</h2>
+    <p><?php echo $paragraph ?></p>
+    <h3>Length:</h3>
+    <p><?php echo strlen($paragraph) ?> characters</p>
+    <h2>The word you wish to censor is:</h2>
+    <p><?php echo $censor_word ?></p>
+    <h2>Here is the censored paragraph:</h2>
+    <p><?php echo $censored_paragraph ?></p>
+    <h3>Length:</h3>
+    <p><?php echo strlen($censored_paragraph) ?> characters</p>
+  </main>
 </body>
 </html>
