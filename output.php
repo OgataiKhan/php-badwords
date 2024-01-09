171bb5daaac2f6ed4afb2cbfa477ee57eca7a137
OgataiKhan <badellino.kevin@gmail.com>
2024-01-09 11:26:13 +0100

    feat: manage no value case

--- output.php
+++ output.php
@@ -1,6 +1,6 @@
 <?php
-  $paragraph = trim($_GET['paragraph']);
-  $censor_word = trim($_GET['censorword']);
+  $paragraph = trim($_GET['paragraph']) ?? '';
+  $censor_word = trim($_GET['censorword']) ?? '';
   $censored_paragraph = str_replace($censor_word, '***', $paragraph);
 ?>
 
@@ -23,15 +23,15 @@
   <main>
     <h1>PHP Badwords</h1>
     <h2>The paragraph you entered is:</h2>
-    <p><?php echo $paragraph ?></p>
+    <p><?php echo $paragraph; ?></p>
     <h3>Length:</h3>
-    <p><?php echo strlen($paragraph) ?> characters</p>
+    <p><?php echo strlen($paragraph); ?> characters</p>
     <h2>The word you wish to censor is:</h2>
-    <p><?php echo $censor_word ?></p>
+    <p><?php echo $censor_word; ?></p>
     <h2>Here is the censored paragraph:</h2>
-    <p><?php echo $censored_paragraph ?></p>
+    <p><?php echo $censored_paragraph; ?></p>
     <h3>Length:</h3>
-    <p><?php echo strlen($censored_paragraph) ?> characters</p>
+    <p><?php echo strlen($censored_paragraph); ?> characters</p>
   </main>
 </body>
 </html>
